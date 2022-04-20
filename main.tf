3b07f4ae921d55ca742338855615d800bd072ba0
chetanchatrath <45016862+chetanchatrath@users.noreply.github.com>
2022-04-20 09:19:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@chetanchatrath 
-chetanchatrath
-/
-TFE4
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-TFE4/main.tf
-@chetanchatrath
-chetanchatrath Create main.tf
-Latest commit c358e36 10 minutes ago
- History
- 1 contributor
-26 lines (22 sloc)  540 Bytes
-   
 terraform {
   required_providers {
     azurerm = {
